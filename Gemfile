128fd2b10f5ae5f421fcdc8e4b3ffcb61c05f9cf
MoeSadoon <msadoon590@gmail.com>
2016-02-08 22:50:18 +0000

    paperclip setup, can post image and caption, second test passing

--- Gemfile
+++ Gemfile
@@ -23,6 +23,11 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'paperclip', '~> 4.2'
+
+gem 'simple_form'
+
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -35,6 +40,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+
 end
 
 group :development do
--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -10,5 +10,18 @@ feature 'posts' do
     end
   end
 
+  context 'post added' do
+    scenario 'should display post on posts page' do
+      visit '/'
+      click_link 'Add Post'
+      attach_file('Image', "spec/files/images/coffee.jpg")
+      fill_in 'Caption', with: 'This is some nice java!'
+      click_button 'Create Post'
+      expect(page).to have_content 'This is some nice java'
+      expect(page).to have_css("img[src*='coffee.jpg']")
+      expect(page).not_to have_content 'No posts yet'
+    end
+  end
+
 
 end
