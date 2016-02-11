f477aa749d08373d8e9c2a522b6a01976ee778fd
MoeSadoon <msadoon590@gmail.com>
2016-02-11 19:10:34 +0000

    first user test passed - can see login or register link and not sign out link

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 feature 'users' do
 
   context 'not signed in' do
-    it 'should display sign in or register link' do
+    it 'should display login or register link' do
       visit '/'
-      expect(page).to have_link 'Sign in'
+      expect(page).to have_link 'Login'
       expect(page).to have_link 'Register'
+    end
+
+    it 'should not se sign out link' do
+      visit '/'
+      expect(page).not_to have_link 'Sign out'
     end
   end
 
