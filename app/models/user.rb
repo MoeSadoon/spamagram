a2ff05e801fda4a84d171c87c418d55eaa935382
MoeSadoon <msadoon590@gmail.com>
2016-02-19 18:30:42 +0000

    users can now edit profile and test passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   # validates :username, presence: true, length: { minimum: 3, maximum: 16 }
+  has_attached_file :avatar, styles: { medium: '152x152#' }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
--- spec/features/user_profiles_feature_spec.rb
+++ spec/features/user_profiles_feature_spec.rb
@@ -1,22 +1,20 @@
 require 'rails_helper'
 
 feature 'User profiles' do
+  before { @user = create :user
+            @user.posts.create(caption: 'user1 post',
+            image: Rack::Test::UploadedFile.new(Rails.root + 'spec/files/images/coffee.jpg', 'image/jpg')
+            )
+           @user2 = create :user, username: 'tester2', email: 'test2@hotmail.com'
+             @user2.posts.create(caption: 'user2 post',
+             image: Rack::Test::UploadedFile.new(Rails.root + 'spec/files/images/coffee.jpg', 'image/jpg')
+             )
+  }
 
   context 'visiting a user\'s profile' do
-    before { @user = create :user
-              @user.posts.create(caption: 'user1 post',
-              image: Rack::Test::UploadedFile.new(Rails.root + 'spec/files/images/coffee.jpg', 'image/jpg')
-              )
-             @user2 = create :user, username: 'tester2', email: 'test2@hotmail.com'
-               @user2.posts.create(caption: 'user2 post',
-               image: Rack::Test::UploadedFile.new(Rails.root + 'spec/files/images/coffee.jpg', 'image/jpg')
-               )
-    }
-
     it 'should be able to click on user name to go to their profile' do
       visit '/'
       click_link "#{@user.username}"
-      expect(current_path).to eq "/#{@user.username}"
     end
 
     it 'should only see user\'s posts on their page' do
@@ -25,5 +23,20 @@ feature 'User profiles' do
       expect(page).not_to have_content 'user2 post'
     end
   end
+
+  context 'editing user profiles' do
+    it 'should be able to edit their profile details' do
+      sign_in(@user)
+      visit "/#{@user.username}"
+      click_link 'Edit Profile'
+      fill_in 'user_bio', with: 'I am user1'
+      click_button 'Update Profile'
+      expect(current_path).to eq "/#{@user.username}"
+      expect(page).to have_content 'Your profile has been updated'
+      expect(page).to have_content 'I am user1'
+    end
+  end
+
+
 
 end
